bb149c2784f668dde103a510d35164b204443f8a
aqtjin <aqtjin@outlook.com>
2019-10-17 16:42:12 +0800

    test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import openvino_inference_support
 
-openvino_inference_support.openvino_predict()
+model = openvino_inference_support.Load_OpenVINO_Model("/home/aqtjin/resnet_v1_50/resnet_v1_50.xml", "/home/aqtjin/resnet_v1_50/resnet_v1_50.bin", 0, 4)
+
+openvino_inference_support.openvino_predict(model)
