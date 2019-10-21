45769000f6b7099b8757a21ab44802bc751260e2
aqtjin <aqtjin@outlook.com>
2019-10-21 16:07:26 +0800

    1021

--- openvino_inference_support.pyx
+++ openvino_inference_support.pyx
@@ -6,14 +6,12 @@
 import time
 import numpy as np
 cimport numpy as np
-cimport cython
 from libcpp.vector cimport vector
 from libcpp.string cimport string
 from libc.stdlib cimport malloc, free
 from cython.operator cimport dereference as deref
 
 BATCH_SIZE = 4
-arr = np.random.random_sample(size = [16, 3, 224, 224])
 
 ctypedef fused T:
     int
@@ -30,7 +28,7 @@ cdef extern from "OpenVINOInferenceSupportive.hpp":
     cdef cppclass CTensor[T]:
         CTensor(f_point _data, vector[size_t] _shape)
         CTensor(vector[size_t] _shape)
-        printCTensor()
+        void * getData()
 
 
 cdef extern from "inference_engine/ie_iexecutable_network.hpp":
@@ -44,25 +42,37 @@ cdef extern from "OpenVINOInferenceSupportive.hpp":
         @staticmethod
         CTensor[float]* predictPTR(ExecutableNetwork executable_network, CTensor[float] datatensor)
 
-def openvino_predict(Loadedmodel):
+cdef pointer_to_numpy_array(void * ptr, np.npy_intp size):
+    '''Convert c pointer to numpy array.
+    The memory will be freed as soon as the ndarray is deallocated.
+    '''
+    cdef extern from "numpy/arrayobject.h":
+        void PyArray_ENABLEFLAGS(np.ndarray arr, int flags)
+    cdef np.ndarray[np.float, ndim=1] arr = \
+            np.PyArray_SimpleNewFromData(1, &size, np.NPY_FLOAT32, ptr)
+    PyArray_ENABLEFLAGS(arr, np.OWNDATA)
+    return arr
+
+def openvino_predict(Loadedmodel, data):
     cdef CTensor[float] *input
     cdef CTensor[float] *output
     cdef ExecutableNetwork * model
     cdef int array_size = 1
     cdef vector[size_t] shape
     cdef float [::1] array
+    cdef void * re
 
     cdef unsigned long ptr = Loadedmodel
     model = <ExecutableNetwork *> ptr
     # Create CTensor Shape
-    for s in arr[0:1*BATCH_SIZE].shape:
+    for s in data[0:1*BATCH_SIZE].shape:
         shape.push_back(s)
 
     print("Begin to prepare data")
-    for i in range(arr.shape[0] // BATCH_SIZE):
+    for i in range(data.shape[0] // BATCH_SIZE):
         # From python array or ndarray to CTensor
         time_s2 = time.time()
-        array = np.ascontiguousarray(arr[i*BATCH_SIZE:(i+1)*BATCH_SIZE].flatten(), dtype=np.single)
+        array = np.ascontiguousarray(data[i*BATCH_SIZE:(i+1)*BATCH_SIZE].flatten(), dtype=np.single)
         # for j in range(len(arr[i*BATCH_SIZE:(i+1)*BATCH_SIZE].flatten())):
         #     array[j] = arr[i*BATCH_SIZE:(i+1)*BATCH_SIZE].flatten()[j]
         time_e2 = time.time()
@@ -71,6 +81,8 @@ def openvino_predict(Loadedmodel):
         input = new CTensor[float](&array[0], shape)
         print("Begin here")
         output = OpenVINOInferenceSupportive.predictPTR(deref(model), deref(input))
+        re = output.getData()
+        arr = pointer_to_numpy_array(re, 4)
         print("Predict successful")
 
 def Load_OpenVINO_Model(xml_path, bin_path, deviceType, batchSize):
--- test.py
+++ test.py
@@ -1,5 +1,21 @@
+import cv2
 import openvino_inference_support
+import numpy as np
+import os
+
+path_dir = "/home/aqtjin/image_test/"
+images = os.listdir(path_dir)
+
+data = cv2.imread(path_dir + images[0])
+data = np.transpose(data, (2, 0, 1))
+data = np.expand_dims(data, axis=0)
+images.pop(0)
+for image in images:
+    img = cv2.imread(path_dir + image)
+    img = np.transpose(img, (2, 0, 1))
+    img = np.expand_dims(img, axis=0)
+    data = np.concatenate((data, img), axis=0)
 
 model = openvino_inference_support.Load_OpenVINO_Model("/home/aqtjin/resnet_v1_50/resnet_v1_50.xml", "/home/aqtjin/resnet_v1_50/resnet_v1_50.bin", 0, 4)
 
-openvino_inference_support.openvino_predict(model)
+openvino_inference_support.openvino_predict(model, data)
